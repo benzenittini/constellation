3b604b35bd998a6cdf4e9392a497101fdda3310e
Ben <ben.zenittini@gmail.com>
2023-02-27 14:54:30 -0500

    Better handled 'none' logging.

--- server/src/Logger.ts
+++ server/src/Logger.ts
@@ -5,11 +5,14 @@ import * as winston from 'winston';
 import { formatHyphenatedYYYYMMDD } from '../../common/utilities/DateUtils';
 
 
-export const logger = winston.createLogger({});
+export const logger = winston.createLogger({
+    silent: true
+});
 
 export function initializeLogger(level: string, directory: string) {
     if (level !== 'none') {
         if (process.env.NODE_ENV === 'production') {
+            logger.silent = false;
             logger.add(new winston.transports.File({
                 level: level,
                 filename: path.join(directory, `${formatHyphenatedYYYYMMDD(new Date())}.log`),
@@ -19,8 +22,10 @@ export function initializeLogger(level: string, directory: string) {
                 )
             }));
         } else if (process.env.NODE_ENV !== 'production') {
+            logger.silent = false;
             logger.add(new winston.transports.Console({
                 level: level,
+                silent: false,
                 format: winston.format.combine(
                     winston.format.timestamp(),
                     winston.format.colorize(),
